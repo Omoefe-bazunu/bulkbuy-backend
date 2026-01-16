eeaae4d24b1f7955301eb566191f05dec3316e55
omoefe <raniem57@gmail.com>
2026-01-16 14:13:08 +0100

    kyc

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const { db } = require("./config/firebase");
 const authRoutes = require("./routes/authRoutes");
-const kycRoutes = require("/routes/kycRoutes");
+const kycRoutes = require("./routes/kycRoutes");
 const productRoutes = require("./routes/productRoutes");
 const subscriptionRoutes = require("./routes/subscriptionRoutes");
 
